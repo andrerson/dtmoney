f428c3f2dae28e5acefb88d895932b263e8e030f
Andrerson Freitas <andrersonn@gmail.com>
2021-05-12 00:24:35 -0300

    Modal e Forms - Estilizando modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import { Dashboard } from "./components/Dashboard";
+import Modal from 'react-modal';
+import { useState } from 'react';
 import { Header } from "./components/Header";
 
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 function App() {
+  const [ isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false);
+
+  function handleOpenNewTransactinoModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactinoModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>    
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactinoModal} />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactinoModal} 
+      />
       <Dashboard />
       <GlobalStyle />
     </>
